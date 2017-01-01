52d9e2b83c642720c4c9ebc1a4ad980b0b16da7d
papiro <pierre.pirault@outlook.com>
2017-01-01 18:21:21 -0500

    update to paths submodule of logger added to node_modules removal of reverse proxy logic from intro

--- intro.js
+++ intro.js
@@ -2,59 +2,54 @@
 
 const 
   config = require('./config'),
-  HttpServer = require('./web_server/index'),
-  reverseProxyWebServer = require('./reverse_proxy/frontierman')(config), 
+  HttpServer = require('./lib/HttpServer'),
   path = require('path')
 ,
   noop = () => {}
 ,
-  onError = (err) => {
-    console.log(err)
-  }
-;
-
-const
   /*** DEFINED MIDDLEWARE ***/
-  mwNoop = require('./middleware/noop'),
-  mwRewrite = require('./middleware/rewrite'),
-  mwMimeType = require('./middleware/mimeType'),
-  mwCaching = require('./middleware/caching'),
-  mwRender = require('./middleware/render'),
-  mwServeFile = require('./middleware/fileServer'),
-  mwLogger = require('./middleware/logger'),
-  mwREST = require('./middleware/REST'),
+  mwNoop = require('./lib/middleware/noop'),
+  mwRewrite = require('./lib/middleware/rewrite'),
+  mwMimeType = require('./lib/middleware/mimeType'),
+  mwCaching = require('./lib/middleware/caching'),
+  mwRender = require('./lib/middleware/render'),
+  mwServeFile = require('./lib/middleware/fileServer'),
+  mwLogger = require('./lib/middleware/logger'),
+  mwREST = require('./lib/middleware/REST'),
   /*** DEFAULT MIDDLEWARE ***/
   defaultMiddleware = [mwREST, mwRewrite, mwMimeType, mwCaching, mwRender, mwServeFile, mwLogger]
 ;
 
-const httpServers = config.sites
-  .map( site => {
-    const { host, port, mount, middleware = defaultMiddleware, onError=noop, onRequest=noop, indexFiles = ['index.html'], restApiEndpoints = [] } = site
-    const server = new HttpServer({ 
-      name: host,
-      port, 
-      mount: path.join(config.root, mount),
-      middleware,
-      indexFiles,
-      restApiEndpoints
-    })
-    server.on('error', onError)
-    server.on('request', onRequest)
-    return server
-  })
+const { 
+  name, 
+  port, 
+  mount, 
+  middleware = defaultMiddleware, 
+  onError = noop, 
+  onRequest = noop, 
+  indexFiles = ['index.html'], 
+  restApiEndpoints = [],
+  socketTimeout = 120000
+} = config
+
+const server = new HttpServer({ 
+  name,
+  port, 
+  mount: path.join(mount),
+  middleware,
+  indexFiles,
+  restApiEndpoints
+})
+
+server.timeout = socketTimeout
+
+server.on('error', onError)
+server.on('request', onRequest)
 
 // handle Ctrl+c
 process.on('SIGINT', () => {
-  console.log(/*newline*/)
-  httpServers.forEach( server => {
-    server.close(() => {
-      console.log(`${server.name || 'server'} shut-down gracefully - `)           
-    })
-  })
-  reverseProxyWebServer.close(() => {
-    console.log(`reverse-proxy-server shut-down gracefully - `)           
-  })
-  process.nextTick(() => {
+  server.close(() => {
+    console.log(`${server.name || 'server'} shut-down gracefully - `)           
     process.exit(1)
   })
 })
