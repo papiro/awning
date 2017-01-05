62474c204b92535fd7a72fd0d98873fb79a0413b
papiro <pierre.pirault@outlook.com>
2017-01-05 01:02:32 -0500

    major overhaul - synchronous middleware engine added as the default - middleware order now defined through file naming - middlewares removed: noop, render, rewrite - awning is now a static file server and REST api server - jsml renderer removed

--- intro.js
+++ intro.js
@@ -3,48 +3,46 @@
 'use strict'
 
 const 
-  config = require('./config'),
-  HttpServer = require('./lib/HttpServer'),
+  fs = require('fs'),
   path = require('path')
 ,
-  noop = () => {}
+  logerr = require('logerr')
 ,
-  /*** DEFINED MIDDLEWARE ***/
-  mwNoop = require('./lib/middleware/noop'),
-  mwRewrite = require('./lib/middleware/rewrite'),
-  mwMimeType = require('./lib/middleware/mimeType'),
-  mwCaching = require('./lib/middleware/caching'),
-  mwRender = require('./lib/middleware/render'),
-  mwServeFile = require('./lib/middleware/fileServer'),
-  mwLogger = require('./lib/middleware/logger'),
-  mwREST = require('./lib/middleware/REST'),
-  /*** DEFAULT MIDDLEWARE ***/
-  defaultMiddleware = [mwREST, mwRewrite, mwMimeType, mwCaching, mwRender, mwServeFile, mwLogger]
+  HttpServer = require('./lib/HttpServer'),
+,
+  noop = () => {}
 ;
+
+const middlewareLoader = new Promise( (resolve, reject) => {
+  fs.readdir('./lib/middleware', (err, res) => {
+    if (err) { reject(err) }
+    resolve(res.map( fn => require(`./lib/middleware/${res}`) ))
+  }) 
+})
 
 const { 
   name, 
   port, 
-  mount, 
-  middleware = defaultMiddleware, 
+  root, 
+  api,
   onError = noop, 
   onRequest = noop, 
-  indexFiles = ['index.html'], 
-  restApiEndpoints = [],
   socketTimeout = 120000
-} = config
+} = require('./config')
 
-const server = new HttpServer({ 
-  name,
-  port, 
-  mount: path.join(mount),
-  middleware,
-  indexFiles,
-  restApiEndpoints
+middlewareLoader.then( middleware => {
+  const server = new HttpServer({ 
+    name,
+    port, 
+    root,
+    api,
+    middleware
+  })
+}).catch( err => {
+  throw err 
 })
 
 server.timeout = socketTimeout
-
 server.on('error', onError)
 server.on('request', onRequest)
 
