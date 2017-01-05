6e28f0423154e17e5da62fbf81fa18c68b312981
papiro <pierre.pirault@outlook.com>
2017-01-05 02:29:11 -0500

    logging refined

--- intro.js
+++ intro.js
@@ -3,20 +3,30 @@
 'use strict'
 
 const 
+  paths = {
+    HttpServer: './lib/HttpServer',
+    middleware: './lib/middleware',
+    config: './config',
+    log: 'logs/startup.log'    
+  }
+,
+  config = require(paths.config)
+,
   fs = require('fs'),
   path = require('path')
 ,
-  logerr = require('logerr')
+  Log = require('logerr')(config.logLevel),
+  log = new Log(paths.log)
 ,
-  HttpServer = require('./lib/HttpServer'),
+  HttpServer = require(paths.HttpServer)
 ,
   noop = () => {}
 ;
 
 const middlewareLoader = new Promise( (resolve, reject) => {
-  fs.readdir('./lib/middleware', (err, res) => {
+  fs.readdir(paths.middleware, (err, res) => {
     if (err) { reject(err) }
-    resolve(res.map( fn => require(`./lib/middleware/${res}`) ))
+    resolve(res.map( fn => require(`${paths.middleware}/${fn}`) ))
   }) 
 })
 
@@ -25,31 +35,34 @@ const {
   port, 
   root, 
   api,
+  async = false,
   onError = noop, 
   onRequest = noop, 
   socketTimeout = 120000
-} = require('./config')
+} = config
 
 middlewareLoader.then( middleware => {
+  log.log('...startup')
   const server = new HttpServer({ 
     name,
     port, 
     root,
     api,
+    async,
     middleware
   })
-}).catch( err => {
-  throw err 
-})
+  server.timeout = socketTimeout
+  server.on('error', onError)
+  server.on('request', onRequest)
 
-server.timeout = socketTimeout
-server.on('error', onError)
-server.on('request', onRequest)
-
-// handle Ctrl+c
-process.on('SIGINT', () => {
-  server.close(() => {
-    console.log(`${server.name || 'server'} shut-down gracefully - `)           
-    process.exit(1)
+  // handle Ctrl+c
+  process.on('SIGINT', () => {
+    server.close(() => {
+      console.log(`${server.name || 'server'} shut-down gracefully - `)           
+      process.exit(1)
+    })
   })
+}).catch( err => {
+  log.error(`STARTUP ERROR ${err}`)
+  throw err 
 })
